ff6efdedcdd5e980e5ac098955e467fa3695a514
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-14 19:18:14 +0200

    Bazooka

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	public float speed = 20;
+
+	void Start()
+	{
+		Destroy(gameObject,3f);
+	}
+
+	void Update()
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+	}
+}
